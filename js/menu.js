5d1480b968e86726eec6f05c8e65ca3df9336bc6
jisooyoon <ygs12567@gmail.com>
2022-02-16 19:58:22 +0900

    add(support.css) : 고객 지원 페이지 반응형작업
     support.css  반응형 작업 추가
     support-page.html mark-up finish
     scroll.js 헤더 반응형 작업 추가
     menu.js 기존 제이쿼리 삭제

--- js/menu.js
+++ js/menu.js
@@ -1,21 +1,47 @@
 // 모바일 & 테블릿 menu
-$('.menu-icon__ic03').click(function () {
-  $('.gnb').show();
-  return false;
-});
-$('.gnb__closeBtn').click(function () {
-  $('.gnb').hide();
-  return false;
+
+let windowWidth = window.innerWidth;
+const moMenuBtn = $('.menu-icon__ic03');
+const moMenu = $('.gnb');
+const moMenuCloseBtn = $('.gnb__closeBtn');
+
+moMenuBtn.on({
+  click: function () {
+    moMenu.slideUp();
+    if (windowWidth >= 320) {
+      moMenu.slideDown(600);
+    }
+    if (windowWidth >= 768) {
+      moMenu
+        .css({
+          right: '-100%',
+          height: '100vh',
+        })
+        .stop()
+        .animate({
+          right: 0,
+        });
+    }
+    return false;
+  },
 });
 
-// footer 언어설정  menu
-$('.footer__country-name').click(function () {
-  if ($(this).attr('data-show') === 'false') {
-    $('.footer_other-country').show();
-    $(this).attr('data-show', 'true');
-  } else {
-    $('.footer_other-country').hide();
-    $(this).attr('data-show', 'false');
-  }
-  return false;
+moMenuCloseBtn.on({
+  click: function () {
+    if (windowWidth >= 320) {
+      moMenu.slideUp(600);
+    }
+    if (windowWidth >= 768) {
+      moMenu
+        .css({
+          right: 0,
+          height: '100vh',
+        })
+        .stop()
+        .animate({
+          right: '-100%',
+        });
+    }
+    return false;
+  },
 });
--- js/scroll.js
+++ js/scroll.js
@@ -1,5 +1,7 @@
 'use strict';
 
+/* qucik menu */
+
 const quickBox = document.querySelector('.quick-box');
 const goToTopBtn = quickBox.querySelector('.quick-box__top');
 
@@ -19,5 +21,27 @@ function handleGoTop(event) {
   });
 }
 
-window.addEventListener('scroll', showQuickBox);
+/* header UI*/
+
+const header = document.querySelector('#header');
+const container = document.querySelector('#container');
+
+function headerfixed() {
+  let scrollY = window.scrollY;
+
+  if (scrollY < 500) {
+    header.classList.remove('header--fixed-top');
+    container.classList.remove('container--margin-top');
+    return;
+  }
+  header.classList.add('header--fixed-top');
+  container.classList.add('container--margin-top');
+}
+
+function scrollEvents(event) {
+  showQuickBox();
+  headerfixed();
+}
+
+window.addEventListener('scroll', scrollEvents);
 goToTopBtn.addEventListener('click', handleGoTop);
