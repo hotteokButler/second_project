1921d5688fc24f73bec6918ebd5e6b250d5b2000
jisooyoon <ygs12567@gmail.com>
2022-02-18 00:17:18 +0900

    Add(index-html.js) :  mainSlide 기능 구현
    - mainSlide :  jQuery  플러그인 slick 사용하여 슬라이드 모션 설정
    - mainSlid :
        1. 노출되는 슬라이드에 따라 슬라이드 title 및 text 변경
        2. 노출되는 슬라이드에 따라 페이저 너비 변경

--- js/index-html.js
+++ js/index-html.js
@@ -1,3 +1,128 @@
+//index.html main_slide
+
+const mainSlide = $('.main-slid__img');
+const mainSlideImgs = mainSlide.find('img');
+const mainSlidePause = $('.main-slid__pager-btn').find('.main-slid__pause');
+const mainSlidePlay = $('.main-slid__pager-btn').find('.main-slid__play');
+const mainSlideTitle = $('.main-slid__title');
+const mainSlideDesc = $('.main-slid__desc');
+
+mainSlide.slick({
+  mobileFirst: true,
+  prevArrow: '.main-slid__pager-btn .main-slid__prev',
+  nextArrow: '.main-slid__pager-btn .main-slid__next',
+  autoplay: true,
+  autoplaySpeed: 5000,
+  speed: 1000,
+});
+
+mainSlidePause.click(function () {
+  mainSlide.slick('slickPause');
+});
+mainSlidePlay.click(function () {
+  mainSlide.slick('slickPlay');
+});
+
+const mainSlideTextBox = document.querySelector('.main-slid__txt');
+const mainSlidePrevBtn = document.querySelector('.main-slid__prev');
+const mainSlideNextBtn = document.querySelector('.main-slid__next');
+const slideTitle = mainSlideTextBox.childNodes[3];
+const slideDesc = mainSlideTextBox.childNodes[5];
+let activeSlides = document.querySelectorAll('.slick-track > img');
+//  pager
+const pagerStartNum = document.querySelector('.main-slid__pager-first');
+const pagerFinishNum = document.querySelector('.main-slid__pager-end');
+//pc pager
+const pcPager = document.querySelector('.main-slid__move');
+const pcPagerBar = document.querySelector('.main-slid__move > p');
+const pcPagerWidth = pcPager.getBoundingClientRect().width;
+//mo,tablet pager
+const moPager = document.querySelector('.main-slid__pager');
+const moPagerBar = document.querySelector('.main-slide__pager--move');
+
+let mainSlideImgAndTxt = [
+  {
+    id: '1',
+    titleUrl: 'url(../img/purifier-humidify-cool-n.png)',
+    text: '포름알데히드를 지속적으로 감지하고 파괴 초미세먼지를 99.5% 제거',
+    slideSrc: 'img/main-bn01.jpg',
+  },
+  {
+    id: '2',
+    titleUrl: 'url(../img/dyson-corrale-ms02.png)',
+    text: '지능형 열제어 시스템으로 스마트하게 손상은 반으로 줄이고 정교한 스타일링을 연출해보세요',
+    slideSrc: 'img/main-bn02.jpg',
+  },
+  {
+    id: '3',
+    titleUrl: 'url(../img/dyson-supersonic_ms03.png)',
+    text: '멀티플라이어 기술로 만들어낸 집중된 바람으로 빠른 건조와 정교한 스타일링을 경험하세요',
+    slideSrc: 'img/main-bn03.jpg',
+  },
+  {
+    id: '4',
+    titleUrl: 'url(../img/lightcycle-morph-ms04.png)',
+    text: '스마트한 밝기 조절 시스템과 적은 블루라이트와 편안함을 선사하는 조명의 혁신을 경험하세요',
+    slideSrc: 'img/main-bn04.jpg',
+  },
+];
+
+pagerFinishNum.innerText = String(mainSlideImgAndTxt.length);
+
+function slideTxtSwitchOption(array, index) {
+  slideTitle.style.backgroundImage = array[index].titleUrl;
+  slideDesc.innerText = array[index].text;
+  pagerStartNum.innerText = array[index].id;
+  if (window.innerWidth > 768) {
+    pcPagerBar.style.width = `${(pcPagerWidth / array.length) * (index + 1)}px`;
+    pcPagerBar.style.transition = '800ms width';
+  } else if (window.innerWidth <= 768) {
+    moPagerBar.style.width = `${(moPagerBar / array.length) * (index + 1)}px`;
+    moPagerBar.style.transition = '800ms width';
+  }
+}
+
+function changeSlideTxt() {
+  for (const item of activeSlides) {
+    let currentSlideImg = item.getAttribute('src');
+    let isAreaHidden = item.getAttribute('aria-hidden');
+
+    if (isAreaHidden === 'false') {
+      switch (currentSlideImg) {
+        case mainSlideImgAndTxt[0].slideSrc:
+          slideTxtSwitchOption(mainSlideImgAndTxt, 0);
+          break;
+        case mainSlideImgAndTxt[1].slideSrc:
+          slideTxtSwitchOption(mainSlideImgAndTxt, 1);
+          break;
+        case mainSlideImgAndTxt[2].slideSrc:
+          slideTxtSwitchOption(mainSlideImgAndTxt, 2);
+          break;
+        case mainSlideImgAndTxt[3].slideSrc:
+          slideTxtSwitchOption(mainSlideImgAndTxt, 3);
+          break;
+      }
+    }
+  }
+}
+
+// 요소 변화 감지 설정
+const target = document.querySelector('.slick-track');
+let observer = new MutationObserver((mutations) => {
+  changeSlideTxt();
+});
+
+let config = {
+  childList: true, // 타겟의 하위 요소 추가 및 제거 감지
+  attributes: true, // 타켓의 속성 변경를 감지
+  characterData: false, // 타겟의 데이터 변경 감지
+  subtree: false, // 타겟의 자식 노드 아래로도 모두 감지
+  attributeOldValue: false, // 타겟의 속성 변경 전 속성 기록
+  characterDataOldValue: false, // 타겟의 데이터 변경 전 데이터 기록
+};
+//감지시작
+observer.observe(target, config);
+
 // section info
 
 const v11Complete = $('#dyson-product-v11Complete');
@@ -5,6 +130,7 @@ const v11CompleteHref = v11Complete.find('.info-card__btn-gray');
 
 v11CompleteHref.on({
   click: function () {
-    location.href = '../v11_complete.html';
+    $(location).attr('href', '../v11_complete.html');
+    return false;
   },
 });
--- js/menu.js
+++ js/menu.js
@@ -1,27 +1,22 @@
-// 모바일 & 테블릿 menu
+// 모바일 & 테블릿 .gnb
 
-let windowWidth = window.innerWidth;
+let windowWidth;
 const moMenuBtn = $('.menu-icon__ic03');
 const moMenu = $('.gnb');
 const moMenuCloseBtn = $('.gnb__closeBtn');
 
 moMenuBtn.on({
   click: function () {
-    moMenu.slideUp();
-    moMenu.hide();
-    if (windowWidth >= 320) {
-      moMenu.slideDown(500);
-    }
-    if (windowWidth >= 768) {
-      moMenu
-        .css({
-          right: '-100%',
-          height: '100vh',
-        })
-        .stop()
-        .animate({
-          right: 0,
-        });
+    windowWidth = window.innerWidth;
+    moMenu.removeClass('gnb--animation-slideDown');
+    moMenu.removeClass('gnb--animation-slideUp');
+    moMenu.removeClass('gnb--animation-slideLeft');
+    moMenu.removeClass('gnb--animation-slideRight');
+
+    if (windowWidth < 768) {
+      moMenu.addClass('gnb--animation-slideDown');
+    } else {
+      moMenu.addClass('gnb--animation-slideLeft');
     }
     return false;
   },
@@ -29,21 +24,99 @@ moMenuBtn.on({
 
 moMenuCloseBtn.on({
   click: function () {
-    if (windowWidth >= 320) {
-      moMenu.slideUp(500);
-    }
-    if (windowWidth >= 768) {
-      moMenu
-        .css({
-          right: 0,
-          height: '100vh',
-        })
-        .stop()
-        .animate({
-          right: '-100%',
-        })
-        .hide(600);
+    windowWidth = window.innerWidth;
+    moMenu.removeClass('gnb--animation-slideDown');
+    moMenu.removeClass('gnb--animation-slideUp');
+    moMenu.removeClass('gnb--animation-slideLeft');
+    moMenu.removeClass('gnb--animation-slideRight');
+    if (windowWidth < 768) {
+      moMenu.addClass('gnb--animation-slideUp');
+    } else {
+      moMenu.addClass('gnb--animation-slideRight');
     }
     return false;
+  },
+});
+
+//pc 메인 메뉴
+
+const pcGnb = $('.gnb-pc ul li');
+const pcGnbDepth2 = pcGnb.find('.gnb-pc__depth2');
+
+pcGnb.eq(0).on({
+  mouseenter: function () {
+    pcGnbDepth2.eq(0).stop().slideDown(500).css({ display: 'flex' });
+  },
+  mouseleave: function () {
+    pcGnbDepth2
+      .eq(0)
+      .css({
+        display: 'flex',
+      })
+      .stop()
+      .slideUp(500);
+  },
+});
+
+// footer 언어설정 및 국기 변경
+
+const footerLangSelect = $('.footer_selected-name');
+const footerCountrys = footerLangSelect.next();
+const footerCountryLi = footerCountrys.find('li');
+const countryFlag = $('.footer__country');
+
+footerLangSelect.on({
+  click: function () {
+    footerCountrys.stop().slideUp();
+
+    if (footerCountrys.css('display') === 'none') {
+      footerCountrys.stop().slideDown(70);
+    } else {
+      footerCountrys.stop().slideUp(70);
+    }
+  },
+});
+
+footerCountryLi.on({
+  click: function () {
+    let country = $(this).text();
+    let index = $(this).index();
+
+    if (country !== footerLangSelect.text()) {
+      footerLangSelect.text(country);
+    }
+
+    switch (index) {
+      case 0:
+        countryFlag.css({
+          backgroundPosition: 'right top -80px',
+        });
+        break;
+      case 1:
+        countryFlag.css({
+          backgroundPosition: 'right top -140px',
+        });
+        break;
+      case 2:
+        countryFlag.css({
+          backgroundPosition: 'right top -60px',
+        });
+        break;
+      case 3:
+        countryFlag.css({
+          backgroundPosition: 'right top -40px',
+        });
+        break;
+      case 4:
+        countryFlag.css({
+          backgroundPosition: 'right top -120px',
+        });
+        break;
+      case 5:
+        countryFlag.css({
+          backgroundPosition: 'right top -160px',
+        });
+        break;
+    }
   },
 });
